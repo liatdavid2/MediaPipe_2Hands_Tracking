d9c58412848cab80cff900592a7a8334b586c4e6
liatdavid2 <liatdavid2@gmail.com>
2022-12-20 14:35:49 +0200

    HandTracking.py

--- HandTracking.py
+++ HandTracking.py
@@ -11,6 +11,19 @@ mpDraw = mp.solutions.drawing_utils
 
 pTime = 0
 cTime = 0 
+
+# We need to set resolutions.
+# so, convert them from float to integer.
+frame_width = int(captureDevice.get(3))
+frame_height = int(captureDevice.get(4))
+   
+size = (frame_width, frame_height)
+# Below VideoWriter object will create
+# a frame of above defined The output 
+# is stored in 'filename.avi' file.
+result = cv2.VideoWriter('HandTracking.avi', 
+                         cv2.VideoWriter_fourcc(*'MJPG'),
+                         10, size)
 while True:
     ret, frame = captureDevice.read() 
     imgRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -33,9 +46,14 @@ while True:
     cv2.putText(frame,f'FPS:{int(fps)}',(40,50),cv2.FONT_HERSHEY_COMPLEX,1, \
            (255,0,0),3 )
 
+    # Write the frame into the
+    # file 'filename.avi'
+    result.write(frame)
+
     cv2.imshow('my frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 captureDevice.release()
+result.release()
 cv2.destroyAllWindows()
